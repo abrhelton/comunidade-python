a48509aa48f86bb9e56ba7296d7bdba1e9fd76cd
Helton Abrantes de Souza <38536364+abrhelton@users.noreply.github.com>
2023-09-01 13:01:33 -0300

    __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
-lista_usuarios = ['Helton', 'Lira']
-
 app.config['SECRET_KEY'] = '94fc440a4071f5da8b10eb4555679a2a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:MHSQAOHozgfAeHjhXXsm@containers-us-west-112.railway.app:8073/railway'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
